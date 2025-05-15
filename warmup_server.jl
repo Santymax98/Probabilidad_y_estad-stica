68a18ceace5bf0917fd6eb58791e6bc0992e5dfc
santymax98 <santymax9807@gmail.com>
2025-05-15 11:30:22 -0500

    new file:   Pluto_run.jl 	new file:   binder/Dokerfile 	new file:   binder/postBuild 	deleted:    pluto_server_config.jl 	deleted:    plutoserver.py 	deleted:    postBuild 	deleted:    setup.py 	modified:   warmup_server.jl

--- warmup_server.jl
+++ warmup_server.jl
@@ -1,15 +1,2 @@
-@info("instantiate from setup.py")
-import Pkg
-Pkg.instantiate()
-
-@info("import Pluto from setup.py")
-import Pluto
-
-@info("starting new notebook from setup.py")
-sesh = Pluto.ServerSession(options=Pluto.Configuration.from_flat_kwargs(; include("pluto_server_config.jl")...))
-nb = Pluto.SessionActions.new(sesh; run_async=false)
-
-
-@info("shutting down notebook from setup.py")
-Pluto.SessionActions.shutdown(sesh, nb; async=false)
-@info("setup.py done");
+using Pluto
+Pluto.new_notebook()  # Solo para disparar la precompilación
